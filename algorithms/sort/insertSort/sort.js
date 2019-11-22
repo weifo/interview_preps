3135f418c68695cc2e4406099d686747ee7bfbf4
weifo <1641929622@qq.com>
2019-11-22 19:42:24 +0800

    排序算法

---
+++ algorithms/sort/insertSort/sort.js
@@ -0,0 +1,21 @@
+function sort(arr){
+    let l=arr.length;
+    for(let i=1;i<l;i++){
+        let j=i,
+        target=arr[i];
+        
+        while(j>0&&target<arr[j-1]){
+            arr[j]=arr[j-1]
+            j--;
+        }
+    }
+    console.log(arr.join(' '))
+    return arr
+}
+let test=[
+    [1,4,12,3,8,6,5],
+    [8,2,6,1,12,3,7],
+    [9,10,3,5,2,6,8]
+]
+test.forEach(arr=>sort(arr));
+
